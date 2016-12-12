e312f06bc79e30e3182a0e740707d2c2010675de
Tyler Brown <tyler@tybro.io>
2016-12-11 21:50:07 -0500

    styling

--- server/components/pages/home.jsx
+++ server/components/pages/home.jsx
@@ -18,19 +18,20 @@ export default class HomePage extends Component {
 
         <div className="home-layer layer-main-content">
           <div className="main-content">
-            <img className="avatar" src="http://gravatar.com/avatar/867388b9e54784c6d9e99b485d42aead?s=200" alt="Tyler Brown" />
-            <h1>Tyler Brown</h1>
-            <p>Lover of coffee, fitness, motorcycles, and the outdoors. Software engineer. Father of two cool dudes. Enjoying life in Chattanooga&apos;s Southside.</p>
-            <ul>
+            <img className="home-avatar" src="http://gravatar.com/avatar/867388b9e54784c6d9e99b485d42aead?s=200" alt="Tyler Brown" />
+            <h1 className="home-title">Tyler Brown</h1>
+            <p className="home-bio">Lover of coffee, fitness, motorcycles, and the outdoors. Software engineer. Father of two cool dudes. Enjoying life in Chattanooga&apos;s Southside.</p>
+            <p className="home-links">
+              <a href="mailto:tyler@tybro.io">tyler@tybro.io</a>
+              &nbsp;|&nbsp;
+              <a href="http://resume.tybro.io">résumé</a>
+            </p>
+            <ul className="home-social">
               <li><a href="https://www.instagram.com/tybro0103/"><i className="fa fa-instagram" /></a></li>
               <li><a href="https://github.com/tybro0103/"><i className="fa fa-github" /></a></li>
               <li><a href="https://www.linkedin.com/in/tyler-brown-03367217"><i className="fa fa-linkedin" /></a></li>
               <li><a href="http://stackoverflow.com/users/202875/tybro0103"><i className="fa fa-stack-overflow" /></a></li>
-              <li><a href="https://www.facebook.com/tylermatthewbrown"><i className="fa fa-facebook" /></a></li>
-            </ul>
-            <ul>
-              <li><a href="mailto:tyler@tybro.io">tyler@tybro.io</a></li>
-              <li><a href="http://resume.tybro.io">résumé</a></li>
+              <li><a href="https://www.facebook.com/tybro0103"><i className="fa fa-facebook" /></a></li>
             </ul>
           </div>
         </div>
